3105f359076b871057f4e7c882a0bb8faebb3909
HKyleZhang <hongkaizhang92@gmail.com>
2018-12-12 12:50:19 +0100

    Create KCmetric.R

---
+++ R_script/Sylvioidea_Ultimate/KC/parallel/KCmetric.R
@@ -0,0 +1,24 @@
+args <- commandArgs(TRUE)
+
+if (!require(treespace)) install.packages("treespace")
+if (!require(foreach)) install.packages("foreach")
+if (!require(doParallel)) install.packages("doParallel")
+
+no_cores <- detectCores()
+registerDoParallel(no_cores)
+
+L <- as.numeric(args[1])
+
+files <- list.files(pattern = "*tre")
+foreach(i=files) %dopar% {
+  print(i)
+  name <- strsplit(i, ".", fixed = TRUE)[[1]][1]
+  name <- paste(name, ".kcd", sep = "")
+  trees <- read.tree(i)
+  res <- multiDist(trees, lambda = L)
+  res <- as.matrix(res)
+  kcd <- round(res[2:length(res[,1]),1],8)
+  write.table(kcd, file = name, quote = FALSE, row.names = FALSE, col.names = FALSE)
+}
+
+stopImplicitCluster()
